6c0243a57c78d29c1e20ae35d4f2e18134e6f021
Andressa Karoline <andressakdogado@gmail.com>
2019-09-16 18:28:47 -0300

    Added validations on person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ApplicationRecord
+    validates_presence_of :weight
+    validates_presence_of :gender
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a simple person" do
+    assert_difference 'Person.all.count', 1 do
+      Person.create weight: 53.0, gender: "f"
+    end
+  end
+
+  test "don't create a person without gender" do
+    assert_difference 'Person.all.count', 0 do
+      Person.create weight: 53.0
+    end
+  end
+
+  test "don't create a person without weight" do
+    assert_difference 'Person.all.count', 0 do
+      Person.create gender: "m"
+    end
+  end
+
+  test "don't create a person without weight e gender" do
+    assert_difference 'Person.all.count', 0 do
+      Person.create name: "person", height: "1.60", age: 20, email: "person@123" 
+    end
+  end
+
+  test "don't create a person with name" do
+    assert_difference 'Person.all.count', 0 do
+      Person.create name: "person" 
+    end
+  end
+
+  test "don't create a person with height" do
+    assert_difference 'Person.all.count', 0 do
+      Person.create height: "1.60" 
+    end
+  end
+
+  test "don't create a person with age" do
+    assert_difference 'Person.all.count', 0 do
+      Person.create age: 20 
+    end
+  end
+
+  test "don't create a person with email" do
+    assert_difference 'Person.all.count', 0 do
+      Person.create email: "person@123" 
+    end
+  end
 end
